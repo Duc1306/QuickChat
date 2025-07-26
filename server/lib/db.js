5880b40f2c7aff08610b401a85a00eb62ee70250
Duc1306 <trinhduc1362k3@gmail.com>
2025-07-26 10:13:30 +0700

    fixxx

--- server/lib/db.js
+++ server/lib/db.js
@@ -4,7 +4,7 @@ import mongoose  from "mongoose";
 export const connectDB = async () =>{
     try{
         mongoose.connection.on('connected', ()=> console.log("database connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
     }catch(e){
         console.log(e);
     }
